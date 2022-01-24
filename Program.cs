6d99a134715f3797c6e8a03b10fd7e40293e0545
Your Github User Name <melquanmgreen95@gmail.com>
2022-01-24 14:57:50 -0500

    phase9 done

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int secretNum = new Random().Next(1, 100);
+
+            Console.WriteLine("choose your difficulty level! CHEATER - EASY - MEDIUM - HARD");
+            string levelInput = Console.ReadLine().ToLower();
+
+            int numOfChances = 4;
+
+             if (levelInput == "easy")
+            {
+                numOfChances = 8;
+            }
+            else if (levelInput == "medium")
+            {
+                numOfChances = 6;
+            }
+            else if (levelInput == "hard")
+            {
+                numOfChances = 4;
+            }
+
+             else if (levelInput == "cheater")
+            {
+                numOfChances = 100;
+            }
+
+            for (int i = 0; i < numOfChances; i++)
+            {
+
+
+                Console.WriteLine($"Thers a secret number.... can you guess it?! (try {i + 1})");
+                Console.WriteLine("...whats your guess??");
+                string userAnswer = Console.ReadLine();
+                int UserAnswer = int.Parse(userAnswer);
+
+
+                if (UserAnswer == secretNum)
+                {
+                    Console.WriteLine("Youre Right!! CONGRATS!!!");
+                    break;
+
+                }
+                else
+                {
+                    if (UserAnswer > secretNum)
+                    {
+                        Console.WriteLine("noooo.. TOO HIGH! try agian :(");
+                    }
+                    else
+                    {
+                        Console.WriteLine("noooo.. TOO LOW! try agian :(");
+
+                    }
+                }
+            }
+
+
+
+
+
+        }
+
+
+    }
+
+}
+
